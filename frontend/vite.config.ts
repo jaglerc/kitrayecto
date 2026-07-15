0acce64a6fcf3ed7f8b22bb51031b652b13fe941
jaglerc <jaglerdavidcaviedesciro@gmail.com>
2026-07-14 20:09:10 -0500

    Estructura inicial Fronted

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Kitrayecto App',
+                short_name: 'Kitratecto',
+                description: 'App de logística',
+                icons: [{
+                    src: 'logo-kitrayecto.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                }]
+            }
+        })
+    ],
+})
