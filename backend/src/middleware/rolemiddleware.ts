4c4198c23550610195a315b68db931df14c7ae3e
jaglerc <jaglerdavidcaviedesciro@gmail.com>
2026-07-17 20:32:28 -0500

    Se agregan validaciones para el token y el rol

---
+++ backend/src/middleware/rolemiddleware.ts
@@ -0,0 +1,29 @@
+import type {
+    Request,
+    Response,
+    NextFunction
+} from 'express';
+
+import type { UserRole } from "../modules/auth/auth.types.js";
+
+export const RoleMiddleware = (...rolesPermitidos: UserRole[]) =>
+    (_req: Request, res: Response, next: NextFunction): void => {
+        const user = res.locals.user;
+
+        if (!user) {
+            res.status(401).json({
+                message: "Usuario no autenticado"
+            });
+            return;
+        }
+
+        if (!rolesPermitidos.includes(user.role)) {
+            res.status(403).json({
+                message: "No tiene permisos para realiza esta acción"
+            });
+            return;
+        }
+
+        next();
+    }
+
