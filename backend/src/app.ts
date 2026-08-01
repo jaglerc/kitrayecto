2aa1a78dd132559e81f2c9ccf70ae67c073b8804
jaglerc <jaglerdavidcaviedesciro@gmail.com>
2026-08-01 02:24:01 -0500

    fix(backend): corregir ruta del router de autenticacion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import authRouter from '../src/modules/auth/auth.router.js';
+import authRouter from "./modules/auth/auth.router.js";
 const app = express();
 
 app.use(cors({
