4e4839b8b488bd08dd084e61a12b72a8fc6d2eb0
jaglerc <jaglerdavidcaviedesciro@gmail.com>
2026-07-31 21:16:25 -0500

    Se crea el Home

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import Footer from "../components/ui/Footer";
+import Header from "../components/ui/Header";
+import WelcomeBanner from "../components/ui/WelcomeBanner"
+import QuickActions from "../components/ui/QuickActions"
+
+interface UsuarioGuardado {
+    id: number;
+    nombre: string;
+    role: string;
+}
+export default function Home() {
+    const usuarioGuardado = localStorage.getItem("user");
+
+    const usuario: UsuarioGuardado | null = usuarioGuardado ?JSON.parse(usuarioGuardado) : null;
+
+    return (
+        <div>
+            <Header nombre={usuario.nombre} role={usuario.role} />
+            <main className="px-2 pb-24 pt-24">
+                <div>
+                    <WelcomeBanner nombre={usuario.nombre} />
+
+                    <section className="flex ">
+                        <QuickActions/>
+
+                    </section>
+
+                </div>
+
+            </main>
+            <Footer/>
+        </div>
+    )
+}
